d6edd33c42149ec8d6666c3a948ce3bbe827c877
Maigua-Jairo <jairo.maigua@epn.edu.ec>
2026-04-30 16:48:23 +0000

    Añadir el enlace de la pagina para githubpages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+ 
+  base: '/Taller_DAW_06-07_Maigua_Camacho_Ledesma/', 
   server: {
     host: '0.0.0.0',
   }
