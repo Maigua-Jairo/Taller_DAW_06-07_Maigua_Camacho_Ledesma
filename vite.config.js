0c5b713a199fcca763b47d699a422ed550bbf28d
Maigua-Jairo <jairo.maigua@epn.edu.ec>
2026-04-30 16:58:18 +0000

    Cambios en vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Taller_DAW_06-07_Maigua_Camacho_Ledesma/',
   plugins: [react()],
- 
-  base: '/Taller_DAW_06-07_Maigua_Camacho_Ledesma/', 
   server: {
     host: '0.0.0.0',
   }
